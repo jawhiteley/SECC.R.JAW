0f1f52cef570ccfd3a12f69eba6926f69b7021c0
Jonathan A. Whiteley <jonathan.whiteley@mail.mcgill.ca>
2011-03-29 14:24:12 -0400

    Applied new MSR to ARA & Cyanobacteria Results
    + started building '_Main' analysis script as control center to batch-execute other analyses.
    + Re-ran ARA & Cyanobacteria with new MSD method.
      - MSD is NaN for Time ?!
    - Removed old Heterocyst results (to reflect the fact that they are essentially deprecated).

--- _Main.R
+++ _Main.R
@@ -5,14 +5,25 @@
 ##################################################
 ## INITIALIZE
 rm(list=ls())  # clear memory
-setwd("./ SECC/")	# Project Directory.
+## Project Directory
+if (FALSE) {  # do not run automatically
+  ## Set Working Directory: path in quotes "".
+  setwd("/Users/jonathan/Documents/ My Documents/PhD/Analysis/ SECC/")  # iMac@McGill
+  setwd("/Users/jaw/Documents/ My Documents/ Academic/McGill/PhD/Analysis/ SECC/")  # JAW-MBP
+  setwd("./ SECC/") # relative to my usual default wd in R GUI (Mac).
+  getwd()           # Check that we're in the right place
+}
+
 
 #source("./lib/load.R")  # (re-)load data
 #source("./lib/init.R")  # Initialize - all analysis scripts should start with this.
 
-## GO
+## DO
 source("./SECC-Data-Template.R")    # Generate Template Data Frame & files.
-source("./do.R")    # Perform Analyses
+source("do.R")                    # Perform Analyses
+
+source("ARA.R")               # Acetylene Reduction Assay (N-fixation) Analysis
+source("Cyanobacteria.R")     # Cyanobcateria density Analysis
 
 ## OUTPUT
-source("./lib/out.R")   # Produce Outputs: graphs, reports, export.
+source("lib/out.R")   # Produce Outputs: graphs, reports, export.
