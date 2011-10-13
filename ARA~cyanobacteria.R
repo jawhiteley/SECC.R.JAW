b6efb078208e8079033029d6159c855d72a7e17e
Jonathan A. Whiteley <jonathan.whiteley@mail.mcgill.ca>
2011-10-13 12:12:44 -0300

    Aggregation over Time: more data exploration
    - used new options in SECC_aggregate to aggregate data over all Time points.
      - Not particularly encouraging, but the low values in June may be dampening patterns in August.
      - samples across time are not from the same patches, so there might be other unexplained variation here.
    - A few other small fixes to code in data exploration (usually references to the wrong object)
    - re-did data exploration for Meta-Community Scale aggregation
      - not very encouraging either.
    
    Signed-off-by: Jonathan A. Whiteley <jonathan.whiteley@mail.mcgill.ca>

--- ARA~cyanobacteria.R
+++ ARA~cyanobacteria.R
@@ -160,9 +160,9 @@ SECCa <- within( SECCa, {
                 X <- as.numeric( get(X.col) )
                 Y <- as.numeric( get(Y.col) )
                 Y.use <- Y  # compatibility with older code
-                X.log <- log(X)
+                X.log <- log10(X)
                 X.log[X <= 0] <- 0
-                Y.log <- log(Y)
+                Y.log <- log10(Y)
                 Y.log[Y <= 0] <- 0
 })
 
@@ -265,7 +265,7 @@ ARAcb.plot  <- qplot(X, Y, data = SECCa, group = Chamber,
 ARAcb.plot  <- ARAcb.plot + jaw.ggplot() + ChamberPts + TopLegend  # order matters!
 
 ARAcb.time   <- ARAcb.plot + Time.facets # Faceted by Time pts
-ARAcb.panels <- ARAcb.plot + Time.facets # Full Faceting ***
+ARAcb.panels <- ARAcb.plot + All.facets # Full Faceting ***
 if (DrawExplorationGraphs) {
   print(ARAcb.plot)
   print(ARAcb.time)
@@ -319,7 +319,7 @@ ARA.H2O.log.panels <- ARA.H2O.log + All.facets
 if (DrawExplorationGraphs) {
   print(ARA.H2O.log)
   print(ARA.H2O.log.time)
-  print(ARA.H2O.panels)
+  print(ARA.H2O.log.panels)
 }
 
 
@@ -346,6 +346,62 @@ if (FALSE) {    ## Deprecated scatterplots, broken down in various ways.
 if (DrawExplorationGraphs) {
   with(SECCa, plot3d(X, H2O, Y, size = 6, pch = pt, col = colr, bg = fill) )
   with(SECCa, plot3d(X.log, H2O, Y.log, size = 6, pch = pt, col = colr, bg = fill) )
+}
+
+##==============================================================
+## Averaged over time?
+##  I know the measurements were not taken directly from the same patch,
+##  but I wonder if some of the noise can be reduced by using long-term averages
+##  similar to the way DeLuca pooled several sample together for his data (2007)
+SECCtavg <- SECC_aggregate(SECCa, drop.trt="Time")
+
+## Higher correlations with Moisture now.
+if (DrawExplorationGraphs) {
+  pairplot(SECCtavg[, c("ARA.m", "ARA.g", "H2O", "Cells.m", "Cells.g", "Hcells.m", "Hcells.g", "Stigonema", "Nostoc" )])
+  ## look at log transformations
+  pairplot(SECCtavg[, c('Y', 'Y.log', 'X', 'X.log', 'H2O')],
+           labels=c(Y.col, paste("log(", Y.col, ")"), 
+                    X.col, paste("log(", X.col, ")"), "H2O"
+                    )
+          )
+
+  ## Cleveland Dotplots (Zuur et al. 2009)
+  Dotplot.y <- "Order of observations"
+  op <- par(mfcol=c(2,2))
+  dotchart(SECCtavg$X, ylab=Dotplot.y, xlab=X.label)
+  dotchart(SECCtavg$X.log, ylab=Dotplot.y, xlab=paste("log(", X.label, ")"))
+  dotchart(SECCtavg$Y, ylab=Dotplot.y, xlab=Y.label)
+  dotchart(SECCtavg$Y.log, ylab=Dotplot.y, xlab=paste("log(", Y.label, ")"))
+  par(op)
+}
+
+## Some clear patterns, but still pretty messy
+Tavg.plot  <- qplot(X, Y, data = SECCtavg, group = Chamber,
+                   geom = "point", size = I(3),
+                   colour = Chamber, shape = Chamber,
+                   xlab = X.plotlab,
+                   ylab = Y.plotlab
+                   )
+Tavg.plot  <- Tavg.plot + jaw.ggplot() + ChamberPts + TopLegend  # order matters!
+Tavg.panels <- Tavg.plot + facet_grid(Frag~Position) # Full Faceting ***
+if (DrawExplorationGraphs) {
+  print(Tavg.plot)
+  print(Tavg.panels)
+}
+
+## A lot of flat (or negative) relationships on these log-log plots.
+## I wonder if the low ARA values of the June sample are suppressing patterns in the August sample?
+Tavg.log.plot  <- qplot(X, Y, data = SECCtavg, log="xy", group = Chamber,
+                   geom = "point", size = I(3),
+                   colour = Chamber, shape = Chamber,
+                   xlab = X.plotlab,
+                   ylab = Y.plotlab
+                   )
+Tavg.log.plot  <- Tavg.log.plot + jaw.ggplot() + ChamberPts + TopLegend  # order matters!
+Tavg.log.panels <- Tavg.log.plot + facet_grid(Frag~Position) # Full Faceting ***
+if (DrawExplorationGraphs) {
+  print(Tavg.log.plot)
+  print(Tavg.log.panels)
 }
 
 
@@ -843,43 +899,138 @@ SECCa <- within( SECCa, {
                 X <- as.numeric( get(X.col) )
                 Y <- as.numeric( get(Y.col) )
                 Y.use <- Y  # compatibility with older code
+                X.log <- log10(X)
+                X.log[X <= 0] <- 0
+                Y.log <- log10(Y)
+                Y.log[Y <= 0] <- 0
 })
 
 ##================================================
 ## EXPLORE: PLOTS
 ##================================================
+### Map of point styles for Chamber treatments
 Chamber.map <- plotMap( "Chamber", labels = levels(SECC$Chamber) )
 Chamber.map <- Chamber.map[ levels(SECC$Chamber) %in% Chamber.use, ]
 Chamber.map$label <- factor(Chamber.map$label)
 point <- 21	# 21 for circles with col & bg ; 16 for solid circles
 Chamber.map$pch <- c(21, 16)  # use circles for both treatments
+Chamber.label <- attr(SECC, "labels")[["Chamber"]]
 
+## Set up some plotting options for each data point
+##  only needed for non-ggplot commands
 SECCa <- within( SECCa,{
-	colr = as.character(ifelse(Chamber == Chamber.map$label[1], 
-                               Chamber.map$col[1], 
-                               Chamber.map$col[2] 
-                               )
-    )
-	fill = as.character(ifelse(Chamber == Chamber.map$label[1], 
-                               Chamber.map$bg[1],
-                               Chamber.map$bg[2]
-                               )
-    )
-	pt = ifelse(Chamber == Chamber.map$label[1], 
-			Chamber.map$pch[1], 
-			Chamber.map$pch[2]
-		)
+                colr = ifelse(Chamber == Chamber.map$label[1], 
+                              Chamber.map$col[1], 
+                              Chamber.map$col[2] 
+                              )
+                fill = ifelse(Chamber == Chamber.map$label[1], 
+                              Chamber.map$bg[1], 
+                              Chamber.map$bg[2] 
+                              )
+                pt   = ifelse(Chamber == Chamber.map$label[1], 
+                              Chamber.map$pch[1], 
+                              Chamber.map$pch[2]
+                              )
 })
 
-qplot(X, Y, data = SECCa, color = Chamber, shape = Chamber, facets = Frag ~ Time) + theme_bw() +
-scale_shape_manual(name = "Chamber", values = Chamber.map$pch, breaks = Chamber.map$label, labels = c("Ambient", "Chamber")) + 
-scale_color_manual(name = "Chamber", values = Chamber.map$col, breaks = Chamber.map$label, labels = c("Ambient", "Chamber")) 
-## scale_fill_manual(name = "Chamber", values = Chamber.map$bg, breaks = Chamber.map$label, labels = c("Ambient", "Chamber"))
+### pairplots of several variables of interest: check for collinearity, patterns, etc.
+### see Zuur et al. books
+if (DrawExplorationGraphs) {
+  pairplot(SECCa[, c("ARA.m", "ARA.g", "H2O", "Cells.m", "Cells.g", "Hcells.m", "Hcells.g", "Stigonema", "Nostoc" )])
+  ## look at log transformations
+  pairplot(SECCa[, c('Y', 'Y.log', 'X', 'X.log', 'H2O')],
+           labels=c(Y.col, paste("log(", Y.col, ")"), 
+                    X.col, paste("log(", X.col, ")"), "H2O"
+                    )
+          )
 
-qplot(H2O, Y, data = SECCa, color = Chamber, shape = Chamber, fill = Chamber, facets = Frag ~ Time) + theme_bw() + # log = "y", 
-scale_shape_manual(name = "Chamber", values = Chamber.map$pch, breaks = Chamber.map$label, labels = c("Ambient", "Chamber")) + 
-scale_color_manual(name = "Chamber", values = Chamber.map$col, breaks = Chamber.map$label, labels = c("Ambient", "Chamber")) + 
-scale_fill_manual(name = "Chamber", values = Chamber.map$bg, breaks = Chamber.map$label, labels = c("Ambient", "Chamber"))
+  ## Cleveland Dotplots (Zuur et al. 2009)
+  Dotplot.y <- "Order of observations"
+  op <- par(mfcol=c(2,2))
+  dotchart(SECCa$X, ylab=Dotplot.y, xlab=X.label)
+  dotchart(SECCa$X.log, ylab=Dotplot.y, xlab=paste("log(", X.label, ")"))
+  dotchart(SECCa$Y, ylab=Dotplot.y, xlab=Y.label)
+  dotchart(SECCa$Y.log, ylab=Dotplot.y, xlab=paste("log(", Y.label, ")"))
+  par(op)
+}
 
-with(SECCa, plot3d(H2O, X, Y, col = colr))
+##==============================================================
+### prepare plot theme for points varying by Chamber
+ChamberPts  <- ggPts.SECC(Chamber.map, Chamber.label) 
+TopLegend   <- opts(legend.position = "top", legend.direction = "horizontal")
+Time.facets <- facet_grid(facets = .~Time)
+All.facets  <- facet_grid(facets = Frag~Time) # The only real difference from main data exploration code above
+
+ARAcb.plot  <- qplot(X, Y, data = SECCa, group = Chamber,
+                   geom = "point", size = I(3),
+                   colour = Chamber, shape = Chamber,
+                   xlab = X.plotlab,
+                   ylab = Y.plotlab
+                   )
+ARAcb.plot  <- ARAcb.plot + jaw.ggplot() + ChamberPts + TopLegend  # order matters!
+
+ARAcb.time   <- ARAcb.plot + Time.facets # Faceted by Time pts
+ARAcb.panels <- ARAcb.plot + All.facets # Full Faceting ***
+if (DrawExplorationGraphs) {
+  print(ARAcb.plot)
+  print(ARAcb.time)
+  print(ARAcb.panels)
+}
+
+## log-log 
+ARAcb.log <- qplot(X, Y, data = SECCa, log = "xy", 
+                    group = Chamber, geom = "point", size = I(3),
+                    colour = Chamber, shape = Chamber,
+                    xlab = X.plotlab,
+                    ylab = Y.plotlab
+                    )
+ARAcb.log <- ARAcb.log + ChamberPts + jaw.ggplot() + TopLegend
+
+ARAcb.time.log   <- ARAcb.log + Time.facets 
+ARAcb.panels.log <- ARAcb.log + All.facets 
+if (DrawExplorationGraphs) {
+  print(ARAcb.log)
+  print(ARAcb.time.log)
+  print(ARAcb.panels.log)
+}
+
+## H2O plots
+ARA.H2O <- qplot(H2O, Y, data = SECCa, 
+                    group = Chamber, geom = "point", size = I(3),
+                    colour = Chamber, shape = Chamber,
+                    ylab = Y.plotlab
+                    )
+ARA.H2O <- ARA.H2O + ChamberPts + jaw.ggplot() + TopLegend
+
+ARA.H2O.time   <- ARA.H2O + Time.facets
+ARA.H2O.panels <- ARA.H2O + All.facets
+if (DrawExplorationGraphs) {
+  print(ARA.H2O)
+  print(ARA.H2O.time)
+  print(ARA.H2O.panels)
+}
+
+ARA.H2O.log <- qplot(H2O, Y, data = SECCa, log = "y", 
+                    group = Chamber, geom = "point", size = I(3),
+                    colour = Chamber, shape = Chamber,
+                    ylab = Y.plotlab
+                    )
+ARA.H2O.log <- ARA.H2O.log + ChamberPts + jaw.ggplot() + TopLegend
+
+ARA.H2O.log.time   <- ARA.H2O.log + Time.facets
+ARA.H2O.log.panels <- ARA.H2O.log + All.facets
+if (DrawExplorationGraphs) {
+  print(ARA.H2O.log)
+  print(ARA.H2O.log.time)
+  print(ARA.H2O.log.panels)
+}
+
+
+##==============================================================
+## 3D plot with X, Y & H2O axes (requires rgl package)
+if (DrawExplorationGraphs) {
+  with(SECCa, plot3d(X, H2O, Y, size = 6, pch = pt, col = colr, bg = fill) )
+  with(SECCa, plot3d(X.log, H2O, Y.log, size = 6, pch = pt, col = colr, bg = fill) )
+}
+
 
