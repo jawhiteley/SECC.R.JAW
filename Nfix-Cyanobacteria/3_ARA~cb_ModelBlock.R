df45756519b3be2df08001f10aebad7da8478342
Jonathan A. Whiteley <jonathan.whiteley@mail.mcgill.ca>
2011-12-02 19:31:24 -0400

    N-fix-cyanobacteria Effects & final figures (in progress)
    effects() used to extract higher-order effects of interest
    The resulting objects can be used to add layers to final (ggplot2) figures,
    but I'm still not sure how to successfully combine layers with different aesthetics & mappings
    - some final figures still buggy.

--- Nfix-Cyanobacteria/3_ARA~cb_ModelBlock.R
+++ Nfix-Cyanobacteria/3_ARA~cb_ModelBlock.R
@@ -28,8 +28,8 @@ theme_set(theme_bw())                  # change global ggplot2 theme
 ################################################################
 ## Repeated here for assurance and easy reference
 SECCa <- within( SECCa, {
-                Y.trans <- Y.log  # convenience
-                X.trans <- X.log  # convenience
+                Y.trans <- Y.log  # convenience: log10
+                X.trans <- X.log  # convenience: log10
 })
 ## drop values of X == 0 
 ## - detection errors where I didn't count any cells 
@@ -182,7 +182,7 @@ Y.model  <- Y.lmH
 ## MODEL SELECTION: Which TERMS do I need in the model?
 ##==============================================================
 ## multi-model averaging and inference with glmulti!
-library(glmulti)                       #  v1.0, april 2011
+library(glmulti)                       #  v1.0, april 2011; v1.0.3, Nov 2011
 ## library(MASS)                          # ?
 ## library(leaps)
 ## Note: glmulti performs an exhaustive search of all candidate models.  
@@ -207,7 +207,8 @@ if (T) {
                               marginality = TRUE, method="g", confsetsize=256, 
                               plotty=FALSE, report=TRUE)
   }
-  ARA.glmulti2 <- consensus(ARA.multi, confsetsize=256)
+  ARA.glmulti2 <- consensus(ARA.multi, confsetsize=256) # more models use more memory!
+  rm(ARA.multi)                        # clean-up
   ## save object to speed up loading for future analysis.  This is still a big file: >1 GB!
   save(ARA.glmulti1, ARA.glmulti2, file="./save/ARA-cb.glmulti.R")
 } else {
@@ -215,6 +216,7 @@ if (T) {
   load("./save/ARA-cb.glmulti.R")
 }
 print(ARA.glmulti2)
+## summary(ARA.glmulti2)
 
 ARA.best2 <- as.formula(summary(ARA.glmulti2)$bestmodel)
 ARA.best2lm <- lm(ARA.best2, data=SECCa)
@@ -262,6 +264,8 @@ par(mfrow=c(1,1))
 plot(ARA.glmulti1, type="p")           # red line at ~2 IC units above best model: should consider at least all models below this line.
 plot(ARA.glmulti1, type="w")           # red line where cumulative evidence weight = 95%
 ## plot(ARA.glmulti1, type="r")           # diagnostic plots (windows only?)
+plot(ARA.glmulti1, type="s")           # term weights (v 1.0.3)
+plot(ARA.glmulti2, type="s")           # term weights (v 1.0.3)
 ## sum of relative evidence weights over all models that include each term?
 barplot(ARA.coef1[, "Importance"], horiz=TRUE, names.arg=ARA.coef1$Term, las=2) 
 plot(ARA.glmulti2, type="p")
@@ -328,7 +332,7 @@ print(ARA.est2)
 ## include relevant higher-order interactions as necessary, and feasible
 Y.formula <- ARA.best2
 Y.model <- Y.lmH
-Y.model <- lm(as.formula(summary(ARA.glmulti2)$bestmodel), data=SECCa)
+Y.model <- Y.lmHB
 Y.model <- ARA.best2lm
 
 ## Add mixed effects extensions to avoid violating model assumptions?
@@ -386,20 +390,99 @@ drop1(Y.lmain)
 ## termplot(Y.model, se=T, rug=T, partial.resid=T)
 library(effects)
 plot(allEffects(Y.model), ask=FALSE)   # interesting, but messy
+
+X.eff <- effect("Chamber:X.trans", Y.model)
+X.TCP.eff <- effect("Time : Chamber : Position : X.trans", Y.model)
+X.BTC.eff <- effect("Block : Time : Chamber : X.trans", Y.model)
+F.eff <- effect("Frag", Y.model)
+X.H.eff <- effect("X.trans:H2O:I(H2O^2)", Y.model, default.levels=9)
+X.HB.eff <- effect("Block:X.trans:H2O:I(H2O^2)", Y.model,
+                   xlevels=list(Block=1:8, 
+                                H2O=seq(0, max(SECCa$H2O), length.out=4))
+) # I want X.trans continuous, but 4 bins for H2O :(
+plot(X.eff,     ask=FALSE)
+plot(X.TCP.eff, ask=FALSE)
+plot(X.BTC.eff, ask=FALSE)
+plot(F.eff,     ask=FALSE)
+plot(X.H.eff,   ask=FALSE)
+plot(X.HB.eff,   ask=FALSE)
+
 plot(effect("X.trans", Y.model), ask=FALSE) # Warning: averaged over interactions
-plot(effect("X.trans:H2O", Y.model), ask=FALSE) # ?
-plot(effect("X.trans*H2O*I(H2O^2)", Y.model), ask=FALSE) # ?
-plot(effect("Chamber:X.trans", Y.model), ask=FALSE)
 plot(effect("Block:X.trans", Y.model), ask=FALSE)
 plot(effect("Block:Time", Y.model), ask=FALSE)
 plot(effect("Time*Chamber", Y.model), ask=FALSE)
 plot(effect("I(H2O^2)", Y.model), ask=FALSE) # wtf?
 plot(effect("H2O", Y.model), ask=FALSE) # wtf?
 
-avPlots(Y.model, terms= ~ X.trans * I(H2O^2), ask=FALSE)
+if (F) {
+  plot(X.eff$x$X.trans, X.eff$fit, ylim=range(c(X.eff$lower, X.eff$upper)), type="n")
+  X.eff.ambient <- which(X.eff$x$Chamber=="Ambient")
+  lines(X.eff$x$X.trans[ X.eff.ambient], X.eff$fit[   X.eff.ambient], lty=1, col="black")
+  lines(X.eff$x$X.trans[ X.eff.ambient], X.eff$lower[ X.eff.ambient], lty=2, col="black")
+  lines(X.eff$x$X.trans[ X.eff.ambient], X.eff$upper[ X.eff.ambient], lty=2, col="black")
+  lines(X.eff$x$X.trans[-X.eff.ambient], X.eff$fit[  -X.eff.ambient], lty=1, col="red4")
+  lines(X.eff$x$X.trans[-X.eff.ambient], X.eff$lower[-X.eff.ambient], lty=2, col="red4")
+  lines(X.eff$x$X.trans[-X.eff.ambient], X.eff$upper[-X.eff.ambient], lty=2, col="red4")
+  ## effect method for glmulti objects?
+  ## getS3method("effect", "lm")
+  ## analyze.model? debug(effects), then call "analyze.model" while debugging
+  ARA.X.eff <- effect.glmulti("X.trans", ARA.glmulti1)
+  ARA.X.eff <- effect("X.trans", Y.model)
+}
 
+##================================================
+## Partial regression
+##================================================
 ## http://intersci.ss.uci.edu/wiki/index.php/R_partial_regression_plots ??
+avPlots(Y.lmH, terms= ~ X.trans * I(H2O^2), ask=FALSE) # car
+avPlots(Y.model, terms= ~ X.trans * I(H2O^2), ask=FALSE) # car
 
+if (F) {
+  ## using update() gives same output as avPlots, above
+  ARA.part <- update(Y.model, .~. -X.trans)
+  cb.part  <- update(Y.model, X.trans ~ . -X.trans)
+  ## Removing interaction terms gives a slightly different result (positive relationship)
+  ARA.part <- update(Y.model, .~. -X.trans -X.trans:H2O -X.trans:I(H2O^2) -Block:X.trans -Chamber:X.trans)
+  cb.part  <- update(Y.model, X.trans ~ . -X.trans -X.trans:H2O -X.trans:I(H2O^2) -Block:X.trans -Chamber:X.trans)
+  ## Partial out higher-order interactions (cleaner +ve relationship)
+  ARA.part <- lm(Y.trans ~ H2O * I(H2O^2) + Block * Time * Chamber * Position + Frag, data=SECCa)
+  cb.part  <- lm(X.trans ~ H2O * I(H2O^2) + Block * Time *Chamber*Position + Frag, data=SECCa)
+  ## dynamic (simple interactions)
+  terms.drop <- grep("X\\.trans(:.+)?", value=TRUE, 
+                     strsplit(as.character(formula(Y.model)[3]), " \\+ ")[[1]] )
+  terms.drop <- paste(terms.drop, collapse=" -")
+  ARA.part <- paste("update(Y.model, .~. -", terms.drop, ")", sep="")
+  cb.part  <- paste("update(Y.model, X.trans ~. -", terms.drop, ")", sep="")
+}
+
+## Removing main + interaction terms dynamically
+RHS.part <- as.character(formula(Y.model)[3]) 
+RHS.part <- gsub("\\s?\\*?\\s?X\\.trans\\s?\\*?\\s?", "", RHS.part) # *-notation
+RHS.part <- gsub("\\+\\+", "+", RHS.part) # leftovers
+RHS.part <- gsub(" [^ ]+\\:\\+", "", RHS.part) # :-notation
+RHS.part <- gsub(" \\+ [^ ]+:$", "", RHS.part) # :-notation
+ARA.part <- paste("update(Y.model, .~ ", RHS.part, ")", sep="")
+cb.part  <- paste("update(Y.model, X.trans ~ ", RHS.part, ")", sep="")
+ARA.part <- eval(parse(text=ARA.part))
+cb.part  <- eval(parse(text=cb.part))
+
+ARA.re   <- resid(ARA.part, type="response")
+cb.re    <- resid(cb.part, type = "response")
+ARA.cb   <- lm(ARA.re ~ cb.re)
+x.ord <- order(cb.re)
+ARA.cb.pred <- predict(ARA.cb, interval="confidence", level=0.95) # 95% CI bands
+
+plot(cb.re, ARA.re, pch=20)
+## points(cb.re[SECCa$Chamber=="Full Chamber"], ARA.re[SECCa$Chamber=="Full Chamber"], pch=19, col="red4")
+## abline(ARA.cb, col="red")
+## 95% CI?
+lines(cb.re[x.ord], ARA.cb.pred[x.ord, 1], col="red", lty=1, lwd=2)
+lines(cb.re[x.ord], ARA.cb.pred[x.ord, 2], col="red", lty=2)
+lines(cb.re[x.ord], ARA.cb.pred[x.ord, 3], col="red", lty=2)
+## the relationship is visually clearer when partialling out higher-order interactions between Block & other experimental treatments, but the r-squared is lower!
+## i.e. Y.model <- Y.lmH
+summary(ARA.cb)                        # R^2 = 0.04 ! :(
+ARA.cb.r2 <- format(summary(ARA.cb)$adj.r.squared, digits=2)
 
 
 
@@ -408,8 +491,9 @@ avPlots(Y.model, terms= ~ X.trans * I(H2O^2), ask=FALSE)
 ################################################################
 if (Save.results == TRUE && is.null(Save.text) == FALSE) {
   capture.output(cat(Save.header), 
+                 print(ARA.glmulti2),
 				 print(Y.formula),              # model
-				 anova(Y.model),                # model summary
+				 Anova(Y.model),                # model summary
 				 summary(Y.model),              # model summary
 				 cat("\n\n"),                   # for output
 				 cat(Save.end),                 # END OUTPUT #
@@ -436,14 +520,24 @@ Y.pred <- expand.grid(Block    = levels(SECCa$Block) ,
                       X.trans  =seq(0, max(SECCa$X.trans), length.out=10 ),
                       H2O      =seq(0, max(SECCa$H2O), length.out=10 ) 
                       )
-Y.pred$predicted <- predict(Y.model, newdata=Y.pred, type="response" )  # newdata must have same explanatory variable names for predict to work.
+Y.pred.response <- predict(Y.model, newdata=Y.pred, type="response", interval="confidence", level=0.95)  # newdata must have same explanatory variable names for predict to work.
+Y.pred$predicted <- Y.pred.response[, 1]
+Y.pred$pred.lwr  <- Y.pred.response[, 2]
+Y.pred$pred.upr  <- Y.pred.response[, 3]
 Y.pred.terms <- predict(Y.model, type="terms")
 ## type=="response" for full predictions (including interactions)
 ## type=="terms" for partial predictions (?)
 
 ## Note: there is a predict() method for glmulti objects...
+Y.multipred <- predict(ARA.glmulti2, se.fit=TRUE)
+Y.pred$multi.fit <- Y.multipred$averages[1]
+Y.pred$multi.lwr <- Y.multipred$averages[1] - Y.multipred$variability[, "+/- (alpha=0.05)"]
+Y.pred$multi.upr <- Y.multipred$averages[1] + Y.multipred$variability[, "+/- (alpha=0.05)"]
 ## Use effect() to get predicted effects for specific terms
 ## summary(eff.obj$term) : $effect, $lower, $upper for 95% CI
+
+if (F) {                               # average predictions for subsets of terms?
+}
 
 
 Chamber.map <- plotMap( "Chamber", labels = levels(SECC$Chamber) )
@@ -469,7 +563,7 @@ SECCa <- within( SECCa,{
 		)
 })
 
-
+## spaghetti plots (too many other terms)
 par(mfrow=c(1,1))
 pred.Y <- with( Y.pred, 
                aggregate(cbind(predicted), list(Chamber = Chamber, X = X.trans), mean)
@@ -497,30 +591,97 @@ pred.Y <- with( Y.pred,
 Chamber.label <- attr(SECC, "labels")[["Chamber"]]
 ChamberPts  <- ggPts.SECC(Chamber.map, Chamber.label) 
 TopLegend   <- opts(legend.position = "top", legend.direction = "horizontal")
+X.label <- paste("\"", attr(SECCa, "labels")[X.col], ": \"*log[10](", attr(SECCa, "units")[X.col], ")", sep="")
+Y.label <- paste("\"", attr(SECCa, "labels")[Y.col], ": \"*log[10](", attr(SECCa, "units")[Y.col], ")", sep="")
+X.label <- parse(text=X.label)
+Y.label <- parse(text=Y.label)
 
-ARA.plot <- qplot(X.trans, Y.trans, data=SECCa, group = Chamber,
-                   geom = "point", size = I(3),
-                   colour = Chamber, shape = Chamber,) + jaw.ggplot() + ChamberPts + TopLegend
-ARA.facet.plot <- ARA.plot + facet_grid(facets=Position~Time)
-ARA.Block.plot <- ARA.plot + facet_grid(facets=Block~Time)
+eff.layers <- function(effObj, conf.int=TRUE) {
+  eff.df <- data.frame(fit     = effObj$fit,
+                       lower   = effObj$lower,
+                       upper   = effObj$upper)
+  for (v in names(effObj$x)) {
+    eff.df[, v] <- effObj$x[, v]
+  }
+  ## no $ in aes() !!!!
+  result <- list(geom_line(data=eff.df, aes(x=X.trans, y=fit, group=NULL, colour="black", shape=NULL)) )
+  if (conf.int == TRUE) {
+    result <- c(result, 
+                geom_line(data=eff.df, aes(x=X.trans, y=lower, group=NULL, colour="black", shape=NULL, lty=2)), 
+                geom_line(data=eff.df, aes(x=X.trans, y=upper, group=NULL, colour="black", shape=NULL, lty=2)) 
+                )
+  }
+  result
+}
+eff.C.layers <- function(effObj, conf.int=TRUE) {
+  eff.df <- data.frame(fit     = effObj$fit,
+                       lower   = effObj$lower,
+                       upper   = effObj$upper)
+  for (v in names(effObj$x)) {
+    eff.df[, v] <- effObj$x[, v]
+  }
+  ## no $ in aes() !!!!
+  result <- list(geom_line(data=eff.df, aes(x=X.trans, y=fit,   group=Chamber)) )
+  if (conf.int == TRUE) {
+    result <- c(result, 
+                geom_line(data=eff.df, aes(x=X.trans, y=lower, group=Chamber, lty=2)), 
+                geom_line(data=eff.df, aes(x=X.trans, y=upper, group=Chamber, lty=2)) 
+                )
+  }
+  result
+}
+eff.F.layers <- function(effObj, conf.int=TRUE) { # for factors
+  eff.df <- data.frame(fit     = effObj$fit,
+                       lower   = effObj$lower,
+                       upper   = effObj$upper)
+  for (v in names(effObj$x)) {
+    eff.df[, v] <- effObj$x[, v]
+  }
+  ## no $ in aes() !!!!
+  result <- list(geom_line(data=eff.df, aes(x=Frag, y=fit)) )
+  if (conf.int == TRUE) {
+    result <- c(result, 
+                geom_line(data=eff.df, aes(x=Frag, y=lower, lty=2)), 
+                geom_line(data=eff.df, aes(x=Frag, y=upper, lty=2)) 
+                )
+  }
+  result
+}
 
-ARA.Frag.plot <- qplot(Frag, Y.trans, data=SECCa, group = Chamber,
-                   geom = "point", size = I(3),
-                   colour = Chamber, shape = Chamber,) + jaw.ggplot() + ChamberPts + TopLegend
-ARA.Frag.plot <- ARA.Frag.plot + facet_grid(facets=.~Time) # optional
+ARA.plot <- qplot(X.trans, Y.trans, data=SECCa, geom = "point", size = I(3),
+                  group = Chamber, colour = Chamber, shape = Chamber,
+                  xlab = bquote(.(X.label)), ylab = bquote(.(Y.label))) + 
+                  jaw.ggplot() + ChamberPts + TopLegend
+ARA.C.plot <- ARA.plot + geom_line(data=X.eff$x, aes(x=X.trans, y=X.eff$fit, group=Chamber)) +
+              geom_line(data=X.eff$x, aes(x=X.trans, y=X.eff$lower, group=Chamber, lty=2)) +
+              geom_line(data=X.eff$x, aes(x=X.trans, y=X.eff$upper, group=Chamber, lty=2))
+ARA.C.plot <- ARA.plot + eff.C.layers(X.eff)
+ARA.facet.plot <- ARA.plot + eff.C.layers(X.TCP.eff) + facet_grid(facets=Position~Time)
+ARA.Block.plot <- ARA.plot + eff.C.layers(X.BTC.eff, conf=F) + facet_grid(facets=Block~Time)
 
-H2O.breaks   <- seq(0, max(SECCa$H2O), length.out=10) #  9 groups
-H2O.bins     <- cut(SECCa$H2O, breaks=H2O.breaks)
-SECCa$H2Obin <- H2O.bins
-ARA.H2O.plot <- qplot(X.trans, Y.trans, data=SECCa, group = Chamber,
-                   geom = "point", size = I(3),
-                   colour = Chamber, shape = Chamber,) + jaw.ggplot() + ChamberPts + TopLegend
-ARA.H2O.plot <- ARA.H2O.plot + facet_wrap(~ H2Obin)
+ARA.Frag.plot <- qplot(Frag, Y.trans, data=SECCa, geom = "point", size = I(3),
+                       group = Chamber, colour = Chamber, shape = Chamber,) + 
+                 jaw.ggplot() + ChamberPts + TopLegend
+ARA.Frag.plot <- ARA.Frag.plot + eff.F.layers(F.eff) + facet_grid(facets=.~Time) # optional
 
+H2O.breaks9   <- seq(0, max(SECCa$H2O), length.out=10) #  9 groups
+H2O.breaks4   <- seq(0, max(SECCa$H2O), length.out=5)  #  4 groups
+SECCa$H2Obin9 <- cut(SECCa$H2O, breaks=H2O.breaks9)
+SECCa$H2Obin4 <- cut(SECCa$H2O, breaks=H2O.breaks4)
+SECCa$H2Obin4 <- factor(SECCa$H2Obin4, levels=rev(levels(SECCa$H2Obin4)) ) # arrange in decreasing order, for top-down rows in plot
+ARA.H2O.plot <- qplot(X.trans, Y.trans, data=SECCa, geom = "point", size = I(3),
+                      group = Chamber, colour = Chamber, shape = Chamber,
+                      xlab = bquote(.(X.label)), ylab = bquote(.(Y.label))) + 
+                jaw.ggplot() + ChamberPts + TopLegend
+ARA.HB.plot  <- ARA.H2O.plot + facet_grid(H2Obin4 ~ Block)
+ARA.H2O.plot <- ARA.H2O.plot + eff.layers(X.H.eff) + facet_wrap(~ H2Obin9)
+
+print(ARA.C.plot)
 print(ARA.facet.plot)
 print(ARA.Block.plot)
 print(ARA.Frag.plot)
 print(ARA.H2O.plot)
+print(ARA.HB.plot)
 
 
 
