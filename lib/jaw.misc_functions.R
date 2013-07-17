df193fed53bcc11082f0ae26617177bc9e586419
Jonathan A. Whiteley <jonathan.whiteley@mail.mcgill.ca>
2013-07-17 07:01:14 -0400

    TODO; start investigating calculating Effect Sizes for nested ANOVA
    Effect Size (ES) for factor = SS_trt / SS_total
    - I just have to figure out how to add that to the summary() output as an additional column (?)
    
    Signed-off-by: Jonathan A. Whiteley <jonathan.whiteley@mail.mcgill.ca>

--- lib/jaw.misc_functions.R
+++ lib/jaw.misc_functions.R
@@ -194,3 +194,153 @@ RegPlot.annote <- function(model, part = c("equation", "pvalue", "r.squared") )
 ###=============================================================
 
 gmean <- function (x) exp( mean( log(x) ) ) # Geometric mean == nth root of product of n values
+
+
+###=============================================================
+## modified summary.aov to include Effect Sizes for factors (SS_factor / SS_total)
+
+summary.jaw <- function (object, intercept = FALSE, split, expand.split = TRUE, 
+    keep.zero.df = TRUE, ...) 
+{
+    splitInteractions <- function(split, factors, names, asgn, 
+        df.names) {
+        ns <- names(split)
+        for (i in unique(asgn)) {
+            if (i == 0 || names[i + 1L] %in% ns) 
+                next
+            f <- rownames(factors)[factors[, i] > 0]
+            sp <- f %in% ns
+            if (any(sp)) {
+                if (sum(sp) > 1) {
+                  old <- split[f[sp]]
+                  nn <- f[sp]
+                  names(nn) <- nn
+                  marg <- lapply(nn, function(x) df.names[asgn == 
+                    (match(x, names) - 1L)])
+                  term.coefs <- strsplit(df.names[asgn == i], 
+                    ":", fixed = TRUE)
+                  ttc <- sapply(term.coefs, function(x) x[sp])
+                  rownames(ttc) <- nn
+                  splitnames <- apply(expand.grid(lapply(old, 
+                    names)), 1L, function(x) paste(x, collapse = "."))
+                  names(splitnames) <- splitnames
+                  tmp <- sapply(nn, function(i) names(old[[i]])[match(ttc[i, 
+                    ], marg[[i]])])
+                  tmp <- apply(tmp, 1L, function(x) paste(x, 
+                    collapse = "."))
+                  new <- lapply(splitnames, function(x) match(x, 
+                    tmp))
+                  split[[names[i + 1L]]] <- new[sapply(new, function(x) length(x) > 
+                    0L)]
+                }
+                else {
+                  old <- split[[f[sp]]]
+                  marg.coefs <- df.names[asgn == (match(f[sp], 
+                    names) - 1L)]
+                  term.coefs <- strsplit(df.names[asgn == i], 
+                    ":", fixed = TRUE)
+                  ttc <- sapply(term.coefs, function(x) x[sp])
+                  new <- lapply(old, function(x) seq_along(ttc)[ttc %in% 
+                    marg.coefs[x]])
+                  split[[names[i + 1L]]] <- new
+                }
+            }
+        }
+        split
+    }
+    asgn <- object$assign[object$qr$pivot[1L:object$rank]]
+    uasgn <- unique(asgn)
+    nterms <- length(uasgn)
+    effects <- object$effects
+    if (!is.null(effects)) 
+        effects <- as.matrix(effects)[seq_along(asgn), , drop = FALSE]
+    rdf <- object$df.residual
+    nmeffect <- c("(Intercept)", attr(object$terms, "term.labels"))
+    coef <- as.matrix(object$coefficients)
+    resid <- as.matrix(object$residuals)
+    wt <- object$weights
+    if (!is.null(wt)) 
+        resid <- resid * wt^0.5
+    nresp <- NCOL(resid)
+    ans <- vector("list", nresp)
+    if (nresp > 1) {
+        names(ans) <- character(nresp)
+        for (y in 1L:nresp) {
+            cn <- colnames(resid)[y]
+            if (is.null(cn) || cn == "") 
+                cn <- y
+            names(ans)[y] <- paste(" Response", cn)
+        }
+    }
+    if (!is.null(effects) && !missing(split)) {
+        ns <- names(split)
+        if (!is.null(Terms <- object$terms)) {
+            if (!is.list(split)) 
+                stop("the 'split' argument must be a list")
+            if (!all(ns %in% nmeffect)) 
+                stop("unknown name(s) in the 'split' list")
+        }
+        if (expand.split) {
+            df.names <- names(coef(object))
+            split <- splitInteractions(split, attr(Terms, "factors"), 
+                nmeffect, asgn, df.names)
+            ns <- names(split)
+        }
+    }
+    for (y in 1L:nresp) {
+        if (is.null(effects)) {
+            nterms <- 0
+            df <- ss <- ms <- numeric(0L)
+            nmrows <- character(0L)
+        }
+        else {
+            df <- ss <- numeric(0L)
+            nmrows <- character(0L)
+            for (i in seq(nterms)) {
+                ai <- (asgn == uasgn[i])
+                df <- c(df, sum(ai))
+                ss <- c(ss, sum(effects[ai, y]^2))
+                nmi <- nmeffect[1 + uasgn[i]]
+                nmrows <- c(nmrows, nmi)
+                if (!missing(split) && !is.na(int <- match(nmi, 
+                  ns))) {
+                  df <- c(df, unlist(lapply(split[[int]], length)))
+                  if (is.null(nms <- names(split[[int]]))) 
+                    nms <- paste("C", seq_along(split[[int]]), 
+                      sep = "")
+                  ss <- c(ss, unlist(lapply(split[[int]], function(i, 
+                    e) sum(e[i]^2), effects[ai, y])))
+                  nmrows <- c(nmrows, paste("  ", nmi, ": ", 
+                    nms, sep = ""))
+                }
+            }
+        }
+        if (rdf > 0) {
+            df <- c(df, rdf)
+            ss <- c(ss, sum(resid[, y]^2))
+            nmrows <- c(nmrows, "Residuals")
+        }
+        nt <- length(df)
+        ms <- ifelse(df > 0L, ss/df, NA)
+        x <- list(Df = df, `Sum Sq` = ss, `Mean Sq` = ms)
+        if (rdf > 0) {
+            TT <- ms/ms[nt]
+            TP <- pf(TT, df, rdf, lower.tail = FALSE)
+            TT[nt] <- TP[nt] <- NA
+            x$"F value" <- TT
+            x$"Pr(>F)" <- TP
+        }
+        class(x) <- c("anova", "data.frame")
+        attr(x, "row.names") <- format(nmrows)
+        if (!keep.zero.df) 
+            x <- x[df > 0, ]
+        pm <- pmatch("(Intercept)", row.names(x), 0L)
+        if (!intercept && pm > 0) 
+            x <- x[-pm, ]
+        ans[[y]] <- x
+    }
+    class(ans) <- c("summary.aov", "listof")
+    attr(ans, "na.action") <- object$na.action
+    ans
+}
+
